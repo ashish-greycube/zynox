10d284dc50a54a23a0fce8e49d9fa7ec17c79668
vr-greycube <vijay@greycube.in>
2021-01-23 02:29:13 +0530

    Added report Customer Bonus

--- zynox/zynox/report/customer_bonus_report/customer_bonus_report.js
+++ zynox/zynox/report/customer_bonus_report/customer_bonus_report.js
@@ -3,7 +3,60 @@
 /* eslint-disable */
 
 frappe.query_reports["Customer Bonus Report"] = {
-	"filters": [
+  filters: [
+    {
+      fieldname: "company",
+      label: __("Company"),
+      fieldtype: "Link",
+      options: "Company",
+      default: frappe.defaults.get_user_default("Company"),
+      reqd: 1,
+    },
+    {
+      fieldname: "from_date",
+      label: __("From Date"),
+      fieldtype: "Date",
+      default: moment().startOf("month").add(-1, "M"),
+      reqd: 1,
+    },
+    {
+      fieldname: "to_date",
+      label: __("To Date"),
+      fieldtype: "Date",
+      default: moment(),
+      reqd: 1,
+    },
+    {
+      fieldname: "customer",
+      label: __("customer"),
+      fieldtype: "Link",
+      options: "Customer",
+    },
+    {
+      fieldname: "customer_group",
+      label: __("Customer Group"),
+      fieldtype: "Link",
+      options: "Customer Group",
+    },
+    {
+      fieldname: "territory",
+      label: __("Territory"),
+      fieldtype: "Link",
+      options: "Territory",
+    },
+    {
+      fieldname: "country",
+      label: __("Country"),
+      fieldtype: "Link",
+      options: "Country",
+    },
 
-	]
+    {
+      fieldname: "group_by",
+      label: __("Group By"),
+      fieldtype: "Select",
+      options: "Item\nCustomer",
+      default: "Item",
+    },
+  ],
 };
--- zynox/zynox/report/customer_bonus_report/customer_bonus_report.py
+++ zynox/zynox/report/customer_bonus_report/customer_bonus_report.py
@@ -2,8 +2,122 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe.utils import cint
+import pandas
+from operator import itemgetter
+
 
 def execute(filters=None):
-	columns, data = [], []
-	return columns, data
+    return get_columns(filters), get_data(filters)
+
+
+def get_columns(filters):
+    if filters.get("group_by") == "Customer":
+        return [
+            dict(
+                label="Customer Name",
+                fieldname="customer",
+                fieldtype="Link",
+                options="Customer",
+                width=200,
+            ),
+            dict(label="Customer Group", fieldname="customer_group", width=200),
+            dict(label="Points", fieldname="points", fieldtype="Float", width=90),
+            dict(label="Sales Partner", fieldname="sales_partner", width=200),
+            dict(label="Territory", fieldname="territory", width=120),
+            # dict(label="Country", fieldname="country", width=120),
+        ]
+    else:
+        return [
+            dict(
+                label="Customer Name",
+                fieldname="customer",
+                fieldtype="Link",
+                options="Customer",
+                width=200,
+            ),
+            dict(
+                label="Item Code",
+                fieldname="item_code",
+                fieldtype="Link",
+                options="Item",
+                width=200,
+            ),
+            dict(label="Item Name", fieldname="item_name", width=200,),
+            dict(label="Qty", fieldname="qty", fieldtype="Float", width=90,),
+            dict(label="Points", fieldname="points", fieldtype="Float", width=90,),
+            # dict(label="Sales Partner", fieldname="sales_partner", width=120),
+        ]
+
+
+def get_conditions(filters):
+    where_clause = ["si.docstatus = 1"]
+
+    if filters.get("from_date"):
+        where_clause.append("si.posting_date >= %(from_date)s")
+    if filters.get("to_date"):
+        where_clause.append("si.posting_date <= %(to_date)s")
+    if filters.get("salesman"):
+        where_clause.append("si.sales_partner = %(salesman)s")
+
+    return " where " + " and ".join(where_clause) if where_clause else ""
+
+
+def get_data(filters):
+    data = []
+    if filters.get("group_by") == "Customer":
+        data = frappe.db.sql(
+            """
+            select 
+                si.sales_partner, sum(sit.qty) qty, si.customer, si.customer_group, si.territory, 
+                sum(coalesce(cgp.qty_to_point_conversion,0)/(sit.qty * ifnull(ucd.conversion_factor,1))) points
+            from 
+                `tabSales Invoice` si
+            inner join 
+                `tabSales Invoice Item` sit on sit.parent = si.name
+            inner join 
+                `tabItem` it on it.item_code = sit.item_code
+            left outer join 
+                `tabCustomer Groupwise Points` cgp on cgp.parent = sit.item_code
+            left outer join 
+                `tabUOM Conversion Detail` ucd on sit.uom <> it.sales_uom and sit.uom = ucd.uom         
+            {where_conditions}
+            group by 
+                si.sales_partner, si.customer, si.customer_group, si.territory
+            """.format(
+                where_conditions=get_conditions(filters)
+            ),
+            filters,
+            as_dict=True,
+            # debug=True,
+        )
+    else:
+        data = frappe.db.sql(
+            """
+            select 
+                si.sales_partner, sit.item_code, sit.item_name, sum(sit.qty) qty,
+                si.customer, si.customer_group, si.territory, 
+                sum(coalesce(cgp.qty_to_point_conversion,0)/(sit.qty * ifnull(ucd.conversion_factor,1))) points
+            from 
+                `tabSales Invoice` si
+            inner join 
+                `tabSales Invoice Item` sit on sit.parent = si.name
+            inner join 
+                `tabItem` it on it.item_code = sit.item_code
+            left outer join 
+                `tabCustomer Groupwise Points` cgp on cgp.parent = sit.item_code
+            left outer join 
+                `tabUOM Conversion Detail` ucd on sit.uom <> it.sales_uom and sit.uom = ucd.uom         
+            {where_conditions}
+            group by 
+                si.sales_partner, sit.item_code, sit.item_name, si.customer, si.customer_group, si.territory
+            """.format(
+                where_conditions=get_conditions(filters)
+            ),
+            filters,
+            as_dict=True,
+            # debug=True,
+        )
+
+    return data
